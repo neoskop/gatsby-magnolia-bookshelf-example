d65045e720ffc1e02b0453f14357d0cb1636f9f8
Maik Jablonski <jablonski@neoskop.de>
2021-04-04 13:31:58 +0200

    Removed boilerplate from Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,32 +2,5 @@ import React from 'react';
 import '../styles/milligram.css';
 
 export default function Layout({ children }) {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="column">
-          <h1>Magnolia Bookcase Demo</h1>
-          <hr />
-        </div>
-      </div>
-      {children}
-      <div className="row">
-        <div className="column">
-          <hr />
-          powered by{' '}
-          <a
-            href="https://www.magnolia-cms.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Magnolia
-          </a>{' '}
-          &amp;{' '}
-          <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
-            Gatsby
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="container">{children}</div>;
 }
